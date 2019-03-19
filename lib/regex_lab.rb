aaf962c71b510a1e8aad78deefe1ba7b269ed6bc
ktochow1 <ktochow1@binghamton.edu>
2019-03-19 01:23:02 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\b\A[:upper:]\z[:punct:]\b/) ? true : false
+  text.match(/^[A-Z].+[\.!?]$/) ? true : false
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
