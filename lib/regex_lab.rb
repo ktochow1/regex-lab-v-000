652d22a1135cc81d488666b936b683640a4d86bc
ktochow1 <ktochow1@binghamton.edu>
2019-03-19 01:28:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,10 +17,3 @@ end
 def valid_phone_number?(phone)
   phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
 end
-
-# def valid_phone_number?(phone)
-#   phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
-# end
-# def valid_phone_number?(phone)
-#   phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
-# end
